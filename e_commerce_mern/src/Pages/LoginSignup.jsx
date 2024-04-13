7fede46a2c00163162e129ab8a5953281369fa0e
Harsh-Metaliya <161742633+Harsh-Metaliya@users.noreply.github.com>
2024-04-13 12:54:36 +0530

    Update LoginSignup.jsx

--- e_commerce_mern/src/Pages/LoginSignup.jsx
+++ e_commerce_mern/src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignup = () => {
   const login = async () => {
     console.log("Login function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login", {
+    await fetch(//Enter you port url for login, {
       method: "POST",
       headers: {
         Accept: "application/form-data",
@@ -37,7 +37,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("signup function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/signup", {
+    await fetch(//Enter you port url for signup, {
       method: "POST",
       headers: {
         Accept: "application/form-data",
