53f1d55d3e1ffc936646ee3bd334f394a65e7ccb
Harsh-Metaliya <161742633+Harsh-Metaliya@users.noreply.github.com>
2024-04-18 09:52:15 +0530

    Update Navbar.jsx

--- e_commerce_mern/src/Components/Navbar/Navbar.jsx
+++ e_commerce_mern/src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>STYLESIZZLE</p>
       </div>
       <img
         style={{ width: "40px" }}
